9a42aa7f94b6861ffb11f14dfc1148b5469ddb6f
halkaabi2 <halkaabi2@csuchico.edu>
2017-10-04 19:58:00 -0700

    ...

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
-#include"bitmap.h"
-#include<string>
-#include<victor>
+#include <iostream>
+#include "bitmap.h"
+#include <string>
+#include <vector>
+using namespace std;
+ int main ()
+  {
+Bitmap image;
+vector <vector <Pixel> > bmp;
+Pixel rgb;
+image.open("machupicchu.bmp");
+bmp = image.toPixelMatrix();
+cout<<"machupicchu.bmp has been loaded. it is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" pixels high."<<endl;
 
-int main() 
-{
-  return 0;
-  }
+         return 0;
+          }
+
