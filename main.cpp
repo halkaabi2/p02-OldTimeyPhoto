54210faa406d7658d59c3e527b4cfd696de71f4b
halkaabi2 <halkaabi2@csuchico.edu>
2017-10-16 20:15:24 -0700

    ...

--- main.cpp
+++ main.cpp
@@ -2,16 +2,89 @@
 #include "bitmap.h"
 #include <string>
 #include <vector>
+
 using namespace std;
- int main ()
-  {
+
+#define OUTPUT_FILENAME "oldtimey.bmp"
+#define WHITE 255
+#define BLACK 0
+#define BORDER_SIZE 5
+#define DOUBLE_BORDER_SIZE BORDER_SIZE * 2
+
+void convertGreyScale(PixelMatrix& pixelMatrix);
+void addBorder(PixelMatrix& pixelMatrix);
+int main()
+{
+
 Bitmap image;
+
 vector <vector <Pixel> > bmp;
-Pixel rgb;
-image.open("machupicchu.bmp");
-bmp = image.toPixelMatrix();
-cout<<"machupicchu.bmp has been loaded. it is "<<bmp[0].size()<<" pixels wide and "<<bmp.size()<<" pixels high."<<endl;
+string filename;
+cout << "Please enter the file name of bitmap: ";
+getline(cin, filename);
+image.open(filename);
 
-         return 0;
-          }
+while (!image.isImage())
+{
+cout << "Please enter the file name of bitmap: ";
+getline(cin, filename);
+image.open(filename);
+}
+PixelMatrix pixelMatrix = image.toPixelMatrix();
+convertGreyScale(pixelMatrix);
+addBorder(pixelMatrix);
+image.fromPixelMatrix(pixelMatrix);
+image.save(OUTPUT_FILENAME);
 
+return 0;
+}
+
+void convertGreyScale(PixelMatrix& pixelMatrix){
+for(unsigned int row = 0; row < pixelMatrix.size(); row++)
+{
+for(unsigned int column=0; column < pixelMatrix[row].size(); column++)
+{
+Pixel current = pixelMatrix[row][column];
+double avg = ((double)(current.red + current.blue + current.green)) /
+3.0;
+
+pixelMatrix[row][column].red = (int)avg;
+pixelMatrix[row][column].blue = (int)avg;
+pixelMatrix[row][column].green = (int)avg;
+}
+}
+}
+void addBorder(PixelMatrix& pixelMatrix){
+for(int row = 0; row < pixelMatrix.size(); row++)
+{
+for(int column=0; column < pixelMatrix[row].size(); column++)
+{
+if (row < DOUBLE_BORDER_SIZE || row > pixelMatrix.size() -
+DOUBLE_BORDER_SIZE ||
+column < DOUBLE_BORDER_SIZE || column > pixelMatrix[row].size() -
+DOUBLE_BORDER_SIZE)
+{
+pixelMatrix[row][column].red = BLACK;
+pixelMatrix[row][column].blue = BLACK;
+pixelMatrix[row][column].green = BLACK;
+}
+}
+}
+
+for(int row = 0; row < pixelMatrix.size(); row++)
+{
+
+for(int column=0; column < pixelMatrix[row].size(); column++)
+{
+
+if (row < BORDER_SIZE || row > pixelMatrix.size() - BORDER_SIZE ||
+column < BORDER_SIZE || column > pixelMatrix[row].size() -
+BORDER_SIZE)
+{
+pixelMatrix[row][column].red = WHITE;
+pixelMatrix[row][column].blue = WHITE;
+pixelMatrix[row][column].green = WHITE;
+}
+}
+}
+}
